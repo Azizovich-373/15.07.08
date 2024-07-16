c0485aeed270c5049504e4a08e343a44b69801b4
Samir <164155512+Azizovich-373@users.noreply.github.com>
2024-07-16 13:21:22 +0500

    main

--- frontend/index.js
+++ frontend/index.js
@@ -2,22 +2,21 @@ import { reload } from "./lib/utils.js";
 import { Student } from "./components/Student.js";
 const main = document.querySelector('.place')
 const form = document.forms.namedItem('add_new')
-const length_num = document.querySelectorAll('tr')
+const base = 'http://localhost:8080'
 form.onsubmit = (e) => {
     e.preventDefault();
     
     let student = {
-        number: length_num.length,
         name: new FormData(form).get('name'),
         age: new FormData(form).get('age'), 
     }
-    fetch('http://localhost:8080/todos', {
+    fetch(base+'/todos', {
         method: "POST",
         body: JSON.stringify(student)
     })
     .then(res => console.log(res))
 }
 
-fetch('http://localhost:8080/todos')
+fetch(base+'/todos')
     .then(res => res.json())
     .then(res => reload(res, main, Student))
